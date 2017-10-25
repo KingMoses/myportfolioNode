1d2e7c31ffb30baa520d05f1e98f3545d2c358db
webdeveloperCD@gmail.com <canceyd@gmail.com>
2017-10-25 16:32:06 -0500

    Yes, pushing to heroku

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.set('views', path.join(__dirname, '../client/src/views'))
 app.use(webRoutes)
 
 // Server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on http://www.localhost:3000/ to stop server press control+c')
 })
